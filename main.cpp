38997127a67e084a0976bb2169340d89896a0796
Ted Song <kitae040522@gmail.com>
2023-11-01 21:10:00 +0900

    feat: 공격한 골대 상태 표시 ([#1](https://github.com/ssu-chocolate-syrup/laser-reflex-game-algorithm/issues/1))

--- main.cpp
+++ main.cpp
@@ -1,150 +1,164 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+    #include <iostream>
+    #include <string>
+    #include <algorithm>
 
-using namespace std;
+    using namespace std;
 
-typedef enum
-{
-    LEFT = 0,
-    DOWN = 1,
-    UP = 2,
-    RIGHT = 3
-} Direction;
-
-constexpr int MAX_X = 7;
-constexpr int MAX_Y = 12;
-
-// X: Left -> Up, Right -> Down
-// Y: Left -> Down, Right -> Up
-constexpr int MirrorTypeX = 1;
-constexpr int MirrorTypeY = 3;
-
-int p1_goalpost = 0;
-int p2_goalpost = 2;
-
-int mirror[MAX_Y][MAX_X];
-int laser[MAX_Y][MAX_X];
-
-Direction mirror_direction(int x, int y, Direction type)
-{
-    if (mirror[x][y] == MirrorTypeX)
+    typedef enum
     {
-        if (type == LEFT) return UP;
-        else if (type == UP) return LEFT;
-        else if (type == RIGHT) return DOWN;
-        else if (type == DOWN) return RIGHT;
+        LEFT = 0,
+        DOWN = 1,
+        UP = 2,
+        RIGHT = 3
+    } Direction;
+
+    constexpr int MAX_X = 7;
+    constexpr int MAX_Y = 10;
+
+    // X: Left -> Up, Right -> Down
+    // Y: Left -> Down, Right -> Up
+    constexpr int MirrorTypeX = 1;
+    constexpr int MirrorTypeY = 3;
+
+    int p1_goalpost[MAX_X] = {-1, -1, -1, 0, 1, -1, -1};
+    int p2_goalpost[MAX_X] = {-1, 1, -1, 0, -1, -1, -1};
+
+    int mirror[MAX_Y][MAX_X];
+    int laser[MAX_Y][MAX_X];
+
+    int get_goalpost_x(const int *user_goalpost_arr)
+    {
+        for (int i = 0; i < MAX_X; ++i) if (user_goalpost_arr[i] == 1) return i;
+        return -1;
     }
-    if (mirror[x][y] == MirrorTypeY)
+
+    Direction mirror_direction(int x, int y, Direction type)
     {
-        if (type == LEFT) return DOWN;
-        else if (type == DOWN) return LEFT;
-        else if (type == RIGHT) return UP;
-        else if (type == UP) return RIGHT;
+        if (mirror[x][y] == MirrorTypeX)
+        {
+            if (type == LEFT) return DOWN;
+            else if (type == DOWN) return LEFT;
+            else if (type == RIGHT) return UP;
+            else if (type == UP) return RIGHT;
+        }
+        if (mirror[x][y] == MirrorTypeY)
+        {
+            if (type == LEFT) return UP;
+            else if (type == UP) return LEFT;
+            else if (type == RIGHT) return DOWN;
+            else if (type == DOWN) return RIGHT;
+        }
     }
-}
 
-pair<int, int> dfs(int x, int y, Direction type)
-{
-    if (x <= -1 || x >= MAX_Y)
+    pair<int, int> dfs(int x, int y, Direction type)
     {
-        if (x == -1) return {x + 1, y};
-        return {x - 1, y};
+        if (x <= -1 || x >= MAX_Y)
+        {
+            if (x == -1) return {x + 1, y};
+            return {x - 1, y};
+        }
+        if (y <= -1 || y >= MAX_X) return {-1, -1}; // 왼쪽 오른쪽 모서리에 도달
+
+        laser[x][y] = 1;
+        if (mirror[x][y]) type = mirror_direction(x, y, type); //거울에 도달했을 경우 방향 전환
+
+        if (type == LEFT) return dfs(x, y - 1, type);
+        else if (type == RIGHT) return dfs(x, y + 1, type);
+        else if (type == DOWN) return dfs(x + 1, y, type);
+        else if (type == UP) return dfs(x - 1, y, type);
+        return {0, 0};
     }
-    if (y <= -1 || y >= MAX_X) return {-1, -1}; // 왼쪽 오른쪽 모서리에 도달
-
-    laser[x][y] = 1;
-    if (mirror[x][y]) type = mirror_direction(x, y, type); //거울에 도달했을 경우 방향 전환
-
-    if (type == LEFT) return dfs(x, y - 1, type);
-    else if (type == RIGHT) return dfs(x, y + 1, type);
-    else if (type == DOWN) return dfs(x + 1, y, type);
-    else if (type == UP) return dfs(x - 1, y, type);
-    return {0, 0};
-}
 
 
-void init()
-{
-    cout << "Please Input Data (x, y, mirror_type)\n";
-    for (int i = 0; i < MAX_Y; ++i) laser[i][MAX_X / 2] = 1;
-}
-
-// x: 레이저의 x 좌표, *p: 승리한 사용자를 구분하기 위한 포인터 변수
-bool goalin(pair<int, int> coordinate, int *p)
-{
-    if (coordinate.first == 0 && coordinate.second == p1_goalpost)
+    void init()
     {
-        *p = 2;
-        return true;
+        for (auto &i: laser) i[MAX_X / 2] = 1;
     }
-    if (coordinate.first == MAX_Y - 1 && coordinate.second == p2_goalpost)
+
+    // x: 레이저의 x 좌표, *p: 승리한 사용자를 구분하기 위한 포인터 변수
+    bool goalin(pair<int, int> coordinate, int *p)
     {
-        *p = 1;
-        return true;
+        if (coordinate.first == 0 && coordinate.second != get_goalpost_x(p1_goalpost)) p1_goalpost[coordinate.second] = 0;
+        if (coordinate.first == MAX_Y - 1 && coordinate.second != get_goalpost_x(p2_goalpost)) p2_goalpost[coordinate.second] = 0;
+
+        if (coordinate.first == 0 && coordinate.second == get_goalpost_x(p1_goalpost))
+        {
+            *p = 2;
+            return true;
+        }
+        if (coordinate.first == MAX_Y - 1 && coordinate.second == get_goalpost_x(p2_goalpost))
+        {
+            *p = 1;
+            return true;
+        }
+        *p = -1;
+        return false;
     }
-    *p = -1;
-    return false;
-}
 
-int encode_input(char mirror_type)
-{
-    if (mirror_type == '\\') return 1;
-    else if (mirror_type == '/') return 3;
-}
-
-// x: 거울을 설치할 x 좌표, 거울을 설치할 y 좌표
-void input(int x, int y, char mirror_type)
-{
-    mirror[x][y] = encode_input(mirror_type);
-}
-
-void pprint()
-{
-    cout << "<< laser >>\n";
-    for (auto &i: laser)
+    int encode_input(char mirror_type)
     {
-        for (int j: i) cout << j << ' ';
+        if (mirror_type == '/') return MirrorTypeX;
+        else if (mirror_type == '\\') return MirrorTypeY;
+    }
+
+    // x: 거울을 설치할 x 좌표, 거울을 설치할 y 좌표
+    void input(int x, int y, char mirror_type)
+    {
+        mirror[x][y] = encode_input(mirror_type);
+    }
+
+    void pprint()
+    {
+        cout << "===========\n";
+        for (int i : p1_goalpost) printf("%c ", !i ? i == -1 ? 'X' : '-' : 'X');
         cout << '\n';
+        for (int i = 0; i < MAX_Y; ++i)
+        {
+            for (int j = 0; j < MAX_X; ++j)
+            {
+                if (mirror[i][j]) printf("%c ", mirror[i][j] == MirrorTypeX ? '/' : '\\');
+                else cout << laser[i][j] << ' ';
+            }
+            cout << '\n';
+        }
+        for (int i : p2_goalpost) printf("%c ", !i ? i == -1 ? 'X' : '-' : 'X');
+        cout << "\n===========\n";
     }
-    cout << "<< Mirror >>\n";
-    for (auto &i: mirror)
-    {
-        for (int j: i) cout << j << ' ';
-        cout << '\n';
-    }
-}
 
-int main()
-{
-    init();
-    string cmd;
-    int x, y, flag = 1;
-    char mirror_type;
-    while (true)
+    int main()
     {
-        int player;
-        pprint();
-        printf("플레이어%d 공격 턴: (install/update/break): ", flag);
-        cin >> cmd;
-        if (cmd == "install")
+        init();
+        string cmd;
+        int x, y, flag = 1;
+        char mirror_type;
+        while (true)
         {
-            cin >> x >> y >> mirror_type;
-            input(x, y, mirror_type);
-        }
-        else if (cmd == "update")
-        {
-            cin >> x >> y;
-            if (mirror[y][x] == MirrorTypeX) input(x, y, MirrorTypeY);
-            else input(x, y, MirrorTypeX);
-        }
-        else if (cmd == "break") break;
-        flag = flag == 1 ? 2 : 1;
-        for (auto &i: laser) for (int &j: i) j = 0;
-        if (goalin(dfs(0, MAX_X / 2, DOWN), &player))
-        {
-            cout << "플레이어" << player << "이 승리했습니다!";
-            break;
+            int player;
+            pprint();
+            printf("플레이어%d 공격 턴: (install/update/break): ", flag);
+            cin >> cmd;
+            if (cmd == "install")
+            {
+                cin >> x >> y >> mirror_type;
+                input(x, y, mirror_type);
+            }
+            else if (cmd == "update")
+            {
+                cin >> x >> y;
+                if (mirror[x][y] == MirrorTypeX) input(x, y, '\\');
+                else input(x, y, '/');
+            }
+            else if (cmd == "break") break;
+            else {
+                cout << "다시 입력해주세요\n";
+                continue;
+            }
+            flag = flag == 1 ? 2 : 1;
+            for (auto &i: laser) for (int &j: i) j = 0;
+            if (goalin(dfs(0, MAX_X / 2, DOWN), &player))
+            {
+                cout << "플레이어" << player << "이 승리했습니다!";
+                break;
+            }
         }
     }
-}
